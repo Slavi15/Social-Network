f678f8c662faee5ca82188ec8d1ff839ee9a94d3
Slavi15 <slavirezashkiofficial@gmail.com>
2025-05-01 22:54:13 +0300

    Update serving html file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
 	build: {
 		outDir: 'dist',
 		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: './index.html'
+			}
+		}
 	},
 	server: {
 		open: true,
