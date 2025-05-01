053719d1861e3873424815811ce43580624b7de2
Slavi15 <slavirezashkiofficial@gmail.com>
2025-05-01 22:48:42 +0300

    Attempt to fix vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
 	base: '/',
+	appType: 'spa',
 	plugins: [react()],
 	build: {
 		outDir: 'dist',
 		emptyOutDir: true,
+	},
+	server: {
+		open: true,
+		hmr: true,
 	},
 })
